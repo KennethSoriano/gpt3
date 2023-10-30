69fe7a3679e33a3d2c046b1f84cfef5964a6d5f7
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-10-30 14:50:06 -0700

    feature styling

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './features.css';
 
-const Features = () => {
-  return (
-    <div>Features</div>
-  )
-}
 
-export default Features
+const Features = () => (
+  <div>
+
+  </div>
+);
+
+export default Features;
